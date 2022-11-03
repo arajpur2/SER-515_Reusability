033b95d8ada5f899e315f6f4937d738c01cee4a7
arajpur2 <arajpur2@asu.edu>
2022-11-03 16:27:56 -0700

    Instructor tests created

---
+++ src/test/java/InstructorTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InstructorTest {
+
+	Instructor instructor = new Instructor();
+
+	@Test
+	void testCreateCourseMenu() {
+		CourseMenu courseMenu = instructor.createCourseMenu(new Course("SER:515", 0), 0);
+		Assertions.assertTrue(courseMenu instanceof HighLevelCourseMenu);
+	}
+
+	@Test
+	void testInstructor() {
+		int expected = 1;
+		assertEquals(expected, instructor.type);
+	}
+
+}
